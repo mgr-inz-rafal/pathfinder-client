79fce0472da3f0a93c085cd89922b0b20ed0fd11
mgr_inz_rafal <rchabowski@gmail.com>
2017-12-31 22:36:35 +0100

    Basic HTTP server functionality using Iron.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
+extern crate iron;
+extern crate bodyparser;
 extern crate pathfinder;
 
 use pathfinder::*;
+use iron::prelude::*;
+use iron::status;
+
 
 fn main() {
+    Iron::new(|x: &mut Request| {
+    let body = x.get::<bodyparser::Raw>();
+    match body {
+        Ok(Some(body)) => println!("Read body:\n{}", body),
+        Ok(None) => println!("No body"),
+        Err(err) => println!("Error: {:?}", err)
+    }        Ok(Response::with((status::Ok, "Hello World!")))
+    }).http("localhost:3000").unwrap();
+    
+    /*
     let test_level = vec![
         1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
         1.0, 0.1, 0.7, 0.1, 0.1, 0.1, 1.0,
@@ -17,4 +32,5 @@ fn main() {
     let result = calculate_shortest_path(width, height, test_level, start, destination);
 
     println!("Reversed path to destination: {}", result);
+    */
 }
