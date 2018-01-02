344059a1746e2b2a508556f87c6111f43cac0c6c
mgr_inz_rafal <rchabowski@gmail.com>
2018-01-02 19:31:20 +0100

    JSON formatted shortest path is sent back to client.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use pathfinder::*;
 use iron::prelude::*;
 use iron::status;
 
+// Share between pathfinder and pathfinder-server
 #[derive(Deserialize)]
 struct PathfinderRequest {
     pub width: u64,
@@ -20,19 +21,33 @@ struct PathfinderRequest {
 }
 
 fn main() {
-    Iron::new(|x: &mut Request| {
-    let body = x.get::<bodyparser::Raw>();
-    match body {
-        Ok(Some(body)) => 
-        {
-            println!("Read body:\n{}\n", body);
-            let deserialized: PathfinderRequest = serde_json::from_str(&body).unwrap();
-            println!("Deserialized field size: {}", deserialized.width * deserialized.height);
-        },
-        Ok(None) => println!("No body"),
-        Err(err) => println!("Error: {:?}", err)
-    }        Ok(Response::with((status::Ok, "Hello World!")))
-    }).http("localhost:3000").unwrap();
+    Iron::new(
+        |x: &mut Request| {
+            let body = x.get::<bodyparser::Raw>();
+            match body {
+                Ok(Some(body)) => 
+                {
+                    // TODO: Decompose to methods
+                    println!("Read body:\n{}\n", body);
+                    let deserialized: PathfinderRequest = serde_json::from_str(&body).unwrap();
+                    println!("Request deserialized correctly [JSON] - field total area: {}", deserialized.field.len());
+                    println!("Invoking pathfinder lib...");
+                    let result = calculate_shortest_path(
+                        deserialized.width,
+                        deserialized.height,
+                        deserialized.field,
+                        deserialized.start,
+                        deserialized.destination);
+                    println!("Calculated path: {}", result);
+                    println!("Sending it back...");
+                    return Ok(Response::with((status::Ok, result)));
+                },
+                Ok(None) => println!("No body"),
+                Err(err) => println!("Error: {:?}", err)
+            }
+        Ok(Response::with((status::Ok, "Hello World!")))
+        }
+    ).http("localhost:3000").unwrap();
 
     /*
     let test_level = vec![
