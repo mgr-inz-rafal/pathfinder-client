22f3afa0509c05c19431ad4eb8c6e73fec316378
mgr_inz_rafal <rchabowski@gmail.com>
2018-01-05 22:18:21 +0100

    Changed the order of paramerters, just for the fun of it

--- tools/post_json.sh
+++ tools/post_json.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-curl -H "Content-Type: application/json" -X POST -d '{"width":7,"height":5,"start":[1,1],"destination":[4,1],"field":[1,1,1,1,1,1,1,1,0.1,0.7,0.1,0.1,0.1,1,1,0.1,0.1,0.1,0.1,0.1,1,1,0.1,0.1,0.1,0.1,0.1,1,1,1,1,1,1,1,1]}' http://localhost:3000/
+curl -H "Content-Type: application/json" -X POST -d '{"width":7,"height":5,"field":[1,1,1,1,1,1,1,1,0.1,0.7,0.1,0.1,0.1,1,1,0.1,0.1,0.1,0.1,0.1,1,1,0.1,0.1,0.1,0.1,0.1,1,1,1,1,1,1,1,1],"start":[1,1],"destination":[4,1]}' http://localhost:3000/
